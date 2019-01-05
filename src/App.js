4abce9943b0096716ea6f741bd1592449792c3b1
Monica <mntamaru@gmail.com>
2019-01-05 11:22:54 -0800

    updated App.js with welcome message

--- src/App.js
+++ src/App.js
@@ -7,18 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to Choose Your Own Adventure!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>
+            Please choose which adventure you would like to take:
+          </p>
+
+          
         </header>
       </div>
     );
