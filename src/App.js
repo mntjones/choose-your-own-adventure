adbb72ec7081e7a4e30d1d0830f210b550974af3
Monica <mntamaru@gmail.com>
2019-01-06 19:47:19 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -2,34 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StorySection from './components/StorySection';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
 
   state = {
     arrayIndex: 0,
+    showBeginning: false,
   }
 
 
   handleClick = (e) => {
-    e.preventDefault();
-    //this.setState
+    this.setState(showBeginning: !this.state.showBeginning)
   }
 
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Welcome to Choose Your Own Adventure!
-          </p>
+      <Router>
+
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Welcome to Choose Your Own Adventure!
+            </p>
+            
           
+            <div className="start">
+              <button onClick={this.handleClick}>Let's Go!</button>
+            </div>
+          </header>
+        </div>
         
-          <div className="start">
-            <button onClick={this.handleClick}>Let's Go!</button>
-          </div>
-        </header>
-      </div>
+      </Router>
     );
   }
 }
