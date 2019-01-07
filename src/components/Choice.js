0d82e729a38e12926bee73ac2b4493250217bc0d
Monica <mntamaru@gmail.com>
2019-01-06 18:53:28 -0800

    added Choice component

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Choice extends Component {
+function Choice(props) {
 
-	
-
-	return (
-
-	
-	)
+		return (
+			<button className="choiceA">this.props.choiceA</button>
+			<button className="choiceB">this.props.choiceB</button>
+		)
 }
 
 export default Choice;
